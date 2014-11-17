04d2e34e306691666324dfc5837d2d34257c51e8
Joe Zhu <sha.joe.zhu@gmail.com>
2014-11-17 20:42:59 +0000

    rework on boost functions

--- src/regular_math.cpp
+++ src/regular_math.cpp
@@ -20,8 +20,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-//#include "regular_math.hpp"
-#include "binomial/binomial.hpp"
+//#includ e "regular_math.hpp"
+//#includ e "binomial/binomial.hpp"
+#include "boost/math/special_functions/binomial.hpp"
 
 double binomial_coefficient ( double n, double k ){
     return boost::math::binomial_coefficient<double> ( n, k);
