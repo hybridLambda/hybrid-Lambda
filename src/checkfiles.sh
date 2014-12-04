ffcdf2f30aba7fdfdb42f451ac63d692e95d8a8c
Joe Zhu <sha.joe.zhu@gmail.com>
2014-12-04 01:29:38 +0000

    use function in checkfiles

--- src/checkfiles.sh
+++ src/checkfiles.sh
@@ -9,60 +9,69 @@ if [ ! -f "${src_dir}/main.cpp" ]; then
   exit 1
 fi
 
+
 OS=`uname -s`
-
-echo "operating system is ${OS}"
-
+#echo "operating system is ${OS}"
+VERSION=$(cat ../version | tr -d '\n')
 echo -n "." # echo "ok not git clone, now check plot and figure" 
+
 #rm master.tar.gz  # just making sure download the one we actually want to 
-plot_dir=${src_dir}/plot
-if [ -f ${plot_dir}/figure.cpp  -a -f ${plot_dir}/figure.hpp  -a -f ${plot_dir}/test_figure.cpp ];then
-	echo -n "." # echo "ok, do nothing" 
-else
-	mkdir plot_tmp_dir
+
+extract_tmp(){
+	mkdir ${tmp}_dir
 	echo -n "." # echo "ok, do something" # 
 
 	if [[ "${OS}" == "Linux" ]]; then
-		wget --no-check-certificate https://github.com/hybridLambda/plot/archive/master.tar.gz -o /dev/null
-	elif [[ "${OS}" == "darwin"* ]]; then
-		curl -LOk https://github.com/hybridLambda/plot/archive/master.zip
+		wget --no-check-certificate https://github.com/hybridLambda/${tmp}/archive/${VERSION}.tar.gz -o /dev/null
+	elif [[ "${OS}" == "Darwin"* ]]; then
+		curl -LOk https://github.com/hybridLambda/${tmp}/archive/master.zip
 	else
-		echo "Unknown OS, fail to download package from https://github.com/hybridLambda/plot/archive/master.tar.gz" 
+		echo "Unknown OS, fail to download package from https://github.com/hybridLambda/${tmp}/archive/master.tar.gz" 
 		exit 1
 	fi
 
 	if [ ! -f master.tar.gz ]; then
-		echo "Error: Download package from https://github.com/hybridLambda/plot/archive/master.tar.gz failed"
+		echo "Error: Download package from https://github.com/hybridLambda/${tmp}/archive/master.tar.gz failed"
 		exit 1
 	fi
-	tar -xf master.tar.gz -C plot_tmp_dir
-	cp plot_tmp_dir/*/* plot/
-	rm -r plot_tmp_dir master.tar.gz
+	tar -xf master.tar.gz -C ${tmp}_dir
+	cp ${tmp}_dir/*/* ${tmp}/
+	rm -r ${tmp}_dir ${VERSION}.tar.gz	
+}
+
+tmp=plot
+tmp_dir=${src_dir}/${tmp}
+if [ -f ${tmp_dir}/figure.cpp  -a -f ${tmp_dir}/figure.hpp  -a -f ${tmp_dir}/test_figure.cpp ];then
+	echo -n "." # echo "ok, do nothing" 
+else
+	extract_tmp()
 fi
 
-freq_dir=${src_dir}/freq
-if [ -f ${freq_dir}/freq.cpp  -a -f ${freq_dir}/freq.hpp  -a -f ${freq_dir}/freq_extra.cpp ];then
+tmp=freq
+tmp_dir=${src_dir}/${tmp}
+if [ -f ${tmp_dir}/freq.cpp  -a -f ${tmp_dir}/freq.hpp  -a -f ${tmp_dir}/freq_extra.cpp ];then
 	echo -n "." # echo "ok, do nothing" 
 else
-	mkdir freq_tmp_dir
-	echo -n "." # echo "ok, do something" # 
+	extract_tmp()
+	#mkdir freq_tmp_dir
+	#echo -n "." # echo "ok, do something" # 
 
-	if [[ "${OS}" == "Linux" ]]; then
-		wget --no-check-certificate https://github.com/hybridLambda/freq/archive/master.tar.gz -o /dev/null
-	elif [[ "${OS}" == "darwin"* ]]; then
-		curl -LOk https://github.com/hybridLambda/freq/archive/master.zip
-	else
-		echo "Unknown OS, fail to download package from https://github.com/hybridLambda/freq/archive/master.tar.gz" 
-		exit 1
-	fi
+	#if [[ "${OS}" == "Linux" ]]; then
+		#wget --no-check-certificate https://github.com/hybridLambda/freq/archive/master.tar.gz -o /dev/null
+	#elif [[ "${OS}" == "Darwin"* ]]; then
+		#curl -LOk https://github.com/hybridLambda/freq/archive/master.zip
+	#else
+		#echo "Unknown OS, fail to download package from https://github.com/hybridLambda/freq/archive/master.tar.gz" 
+		#exit 1
+	#fi
 	
-	if [ ! -f master.tar.gz ]; then
-		echo "Error: Download package from https://github.com/hybridLambda/freq/archive/master.tar.gz failed"
-		exit 1
-	fi
-	tar -xf master.tar.gz -C freq_tmp_dir
-	cp freq_tmp_dir/*/* freq/
-	rm -r freq_tmp_dir master.tar.gz
+	#if [ ! -f master.tar.gz ]; then
+		#echo "Error: Download package from https://github.com/hybridLambda/freq/archive/master.tar.gz failed"
+		#exit 1
+	#fi
+	#tar -xf master.tar.gz -C freq_tmp_dir
+	#cp freq_tmp_dir/*/* freq/
+	#rm -r freq_tmp_dir master.tar.gz
 fi
 
 
