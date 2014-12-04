325ba865d216d6467deb788e49f63a40dcfabddb
Joe Zhu <sha.joe.zhu@gmail.com>
2014-12-04 01:42:01 +0000

    include version for plot and freq

--- src/checkfiles.sh
+++ src/checkfiles.sh
@@ -24,17 +24,18 @@ extract_tmp(){
 	if [[ "${OS}" == "Linux" ]]; then
 		wget --no-check-certificate https://github.com/hybridLambda/${tmp}/archive/${VERSION}.tar.gz -o /dev/null
 	elif [[ "${OS}" == "Darwin"* ]]; then
-		curl -LOk https://github.com/hybridLambda/${tmp}/archive/master.zip
+		curl -LOk https://github.com/hybridLambda/${tmp}/archive/${VERSION}.zip
 	else
-		echo "Unknown OS, fail to download package from https://github.com/hybridLambda/${tmp}/archive/master.tar.gz" 
+		echo "Unknown OS, fail to download package from https://github.com/hybridLambda/${tmp}/archive/${VERSION}.tar.gz" 
 		exit 1
 	fi
 
-	if [ ! -f master.tar.gz ]; then
-		echo "Error: Download package from https://github.com/hybridLambda/${tmp}/archive/master.tar.gz failed"
+	if [ ! -f ${VERSION}.tar.gz ]; then
+		echo "Error: Download package from https://github.com/hybridLambda/${tmp}/archive/${VERSION}.tar.gz failed"
 		exit 1
 	fi
-	tar -xf master.tar.gz -C ${tmp}_dir
+	tar -xf ${VERSION}.tar.gz -C ${tmp}_dir
+	mkdir ${tmp}
 	cp ${tmp}_dir/*/* ${tmp}/
 	rm -r ${tmp}_dir ${VERSION}.tar.gz	
 }
@@ -44,7 +45,7 @@ tmp_dir=${src_dir}/${tmp}
 if [ -f ${tmp_dir}/figure.cpp  -a -f ${tmp_dir}/figure.hpp  -a -f ${tmp_dir}/test_figure.cpp ];then
 	echo -n "." # echo "ok, do nothing" 
 else
-	extract_tmp()
+	extract_tmp
 fi
 
 tmp=freq
@@ -52,42 +53,6 @@ tmp_dir=${src_dir}/${tmp}
 if [ -f ${tmp_dir}/freq.cpp  -a -f ${tmp_dir}/freq.hpp  -a -f ${tmp_dir}/freq_extra.cpp ];then
 	echo -n "." # echo "ok, do nothing" 
 else
-	extract_tmp()
-	#mkdir freq_tmp_dir
-	#echo -n "." # echo "ok, do something" # 
-
-	#if [[ "${OS}" == "Linux" ]]; then
-		#wget --no-check-certificate https://github.com/hybridLambda/freq/archive/master.tar.gz -o /dev/null
-	#elif [[ "${OS}" == "Darwin"* ]]; then
-		#curl -LOk https://github.com/hybridLambda/freq/archive/master.zip
-	#else
-		#echo "Unknown OS, fail to download package from https://github.com/hybridLambda/freq/archive/master.tar.gz" 
-		#exit 1
-	#fi
-	
-	#if [ ! -f master.tar.gz ]; then
-		#echo "Error: Download package from https://github.com/hybridLambda/freq/archive/master.tar.gz failed"
-		#exit 1
-	#fi
-	#tar -xf master.tar.gz -C freq_tmp_dir
-	#cp freq_tmp_dir/*/* freq/
-	#rm -r freq_tmp_dir master.tar.gz
+	extract_tmp
 fi
 
-
-
-#if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        ## ...
-#elif [[ "$OSTYPE" == "darwin"* ]]; then
-        ## Mac OSX
-#elif [[ "$OSTYPE" == "cygwin" ]]; then
-        ## POSIX compatibility layer and Linux environment emulation for Windows
-#elif [[ "$OSTYPE" == "msys" ]]; then
-        ## Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-#elif [[ "$OSTYPE" == "win32" ]]; then
-        ## I'm not sure this can happen.
-#elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        ## ...
-#else
-        ## Unknown.
-#fi
